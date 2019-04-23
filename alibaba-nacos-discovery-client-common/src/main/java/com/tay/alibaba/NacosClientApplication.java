1cee2c7f6b7cb3e0613d21abc1d084454e952a08
陈泰 <chentai@ycda-group.com>
2019-04-23 15:47:42 +0800

    Nacos服务消费者

--- alibaba-nacos-discovery-client-common/src/main/java/com/tay/alibaba/NacosClientApplication.java
+++ alibaba-nacos-discovery-client-common/src/main/java/com/tay/alibaba/NacosClientApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
  * @author chentay
  * @date 2019/04/23
  */
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosClientApplication {
@@ -32,6 +38,12 @@ public class NacosClientApplication {
         @Autowired
         LoadBalancerClient loadBalancerClient;
 
+        @Autowired
+        RestTemplate restTemplate;
+
+        @Autowired
+        Client client;
+
         @GetMapping("/test")
         public String test() {
             // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
@@ -41,6 +53,49 @@ public class NacosClientApplication {
             String result = restTemplate.getForObject(url, String.class);
             return "Invoke : " + url + ", return : " + result;
         }
+
+        /**
+         * RestTemplate消费方式
+         *
+         * @return {@link String}
+         */
+        @GetMapping("/restTemplate")
+        public String restTemplate() {
+            String result = restTemplate.getForObject("http://alibaba-nacos-discovery-server/hello?name=didi", String.class);
+            return "Return : " + result;
+        }
+
+
+        /**
+         * Feign消费方式
+         *
+         * @return {@link String}
+         */
+        @GetMapping("/feign")
+        public String feign() {
+            String result = client.hello("didi");
+            return "Return : " + result;
+        }
+
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @FeignClient("alibaba-nacos-discovery-server")
+    interface Client {
+
+        /**
+         * 实现内部接口调用
+         *
+         * @param name name
+         * @return {@link String}
+         */
+        @GetMapping("/hello")
+        String hello(@RequestParam(name = "name") String name);
+
+    }
 }
